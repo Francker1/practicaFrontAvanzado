edc490a94ed672a7df38d4df05f3b73895619c97
Italo Franco <italo.ravina@gmail.com>
2020-01-20 16:57:50 +0100

    second version cards collapse, add control icons by css instead of js.

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,15 @@
 
 
 /*Effect collapse in cards, add or quit */
-const cardClosed = document.querySelectorAll('.card__secondary a');
+const cardTitle = document.querySelectorAll('.card .card-title');
 
-cardClosed.forEach((header, index) => {
+cardTitle.forEach((header, index) => {
 
     const card = header.parentNode;
-    const cardArrowMore = card.getElementsByClassName('card__icon-more');
-    const cardArrowRemove = card.getElementsByClassName('card__icon-remove');
 
     header.addEventListener('click', evt => {
         evt.preventDefault();
-
-        /*Functionality toggle clases icons and cards*/
         card.classList.toggle('card-closed');
-        cardArrowMore[0].classList.toggle('d-none');
-        cardArrowRemove[0].classList.toggle('d-block');
     });
 
 });
