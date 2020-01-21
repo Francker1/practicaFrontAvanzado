db741705583b16f8bc551e3fefbfee4de0c69e8e
Italo Franco <italo.ravina@gmail.com>
2020-01-21 18:45:07 +0100

    last change search mobile functionality.

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -5,20 +5,21 @@ const searchBarMobile = document.querySelector("#search-mobile-form");
 const searchIconMobile = document.querySelector("#mob-search-icon");
 const closeIconMobile = document.querySelector("#mob-close-icon");
 
-console.log(searchIconMobile);
 
 const handleSearchbar = replace(searchBarMobile);
 const handleSearchIcon = replace(searchIconMobile);
 const handleCloseIcon = replace(closeIconMobile);
 
 searchIconMobile.addEventListener('click', evt =>{
+
     handleSearchbar('no-search', 'search');
     handleSearchIcon('search','no-search');
     handleCloseIcon('no-search', 'search');
 });
 
 closeIconMobile.addEventListener('click', evt => {
+
     handleSearchbar('search', 'no-search');
-    handleCloseIcon('search','no-search');
     handleSearchIcon('no-search', 'search');
+    handleCloseIcon('search','no-search');
 });
